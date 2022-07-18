55c90ca5786655914f80cdc710be8cec878401dc
gabo.mejia.puch <elgabopuch@gmail.com>
2022-07-17 23:59:08 -0400

    Simple Web Application first commit

---
+++ HW2/SimpleWebApp/SimpleWebApp/Controllers/LocalInfoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LocalInfoController : ControllerBase
+    {
+        private readonly ILogger<LocalInfoController> _logger;
+        public LocalInfoController(ILogger<LocalInfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetLocalInfo")]
+        public LocalInfo GetLocalInfo()
+        {
+            var currentDateTime = DateTime.Now;
+            var myHost = Dns.GetHostEntry(Dns.GetHostName());
+            var myIp = string.Empty;
+            foreach (var ipaddr in myHost.AddressList)
+            {
+                if (ipaddr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    myIp = ipaddr.ToString();
+                }
+            }
+
+            return new LocalInfo()
+            {
+                Date = currentDateTime.ToString("MM-dd-yyyy"),
+                Time = currentDateTime.ToString("HH:mm"),
+                TimeZone = TimeZoneInfo.Local.StandardName,
+                HostName = myHost.HostName,
+                IpAddress = myIp
+            };
+        }
+
+    }
+}
---
+++ HW2/SimpleWebApp/SimpleWebApp/LocalInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleWebApp
+{
+    public class LocalInfo
+    {
+        public string Time { get; set; }
+
+        public string Date { get; set; }
+
+        public string TimeZone { get; set;}
+
+        public string HostName { get; set; }
+
+        public string IpAddress { get; set; }
+    }
+}
